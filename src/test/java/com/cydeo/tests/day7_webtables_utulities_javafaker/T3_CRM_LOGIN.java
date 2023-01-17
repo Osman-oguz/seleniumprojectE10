3a787eac8288e4044ad34ed501590d2f039c0b85
Osman Oğuz <osman-oguz@hotmail.com>
2023-01-17 21:25:32 +0300

    Created day9_Added Java Faker & ConfigurationReader

--- src/test/java/com/cydeo/tests/day7_webtables_utulities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utulities_javafaker/T3_CRM_LOGIN.java
@@ -1,25 +1,14 @@
 package com.cydeo.tests.day7_webtables_utulities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utulities.BrowserUtils;
-import com.cydeo.utulities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class T3_CRM_LOGIN extends TestBase {
 
-public class T3_CRM_LOGIN {
 
-    public WebDriver driver;
-
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
          //TC #3: Login scenario
     @Test
     public void crm_login_test(){
