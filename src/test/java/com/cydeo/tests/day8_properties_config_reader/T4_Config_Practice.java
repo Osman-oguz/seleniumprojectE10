a24aec23be6ef47d102e4b1d990ad41fcf9b5d24
Osman Oğuz <osman-oguz@hotmail.com>
2023-01-18 00:43:53 +0300

    Created day9_Driver-Singleton-Testbase

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T4_Config_Practice.java
@@ -1,20 +1,17 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utulities.ConfigurationReader;
-import com.cydeo.utulities.WebDriverFactory;
+import com.cydeo.utulities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class T4_Config_Practice {
 
-    public WebDriver driver;
+
+    /*public WebDriver driver;
 
     @BeforeMethod
     public void setupMethod(){
@@ -24,22 +21,25 @@ public class T4_Config_Practice {
         driver = WebDriverFactory.getDriver(browserType);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.google.com");
+
     }
+
+     */
 
 
     @Test
     public void google_search_test(){
 
+        Driver.getDriver().get("https://www.google.com");
         //3- Write “apple” in search box
-        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement googleSearchBox = Driver.getDriver().findElement(By.xpath("//input[@name='q']"));
 
         googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue")+ Keys.ENTER);
 
         //4- Verify title:
         //Expected: apple - Google Search
         String expectedTitle = ConfigurationReader.getProperty("searchValue") +" - Google Search";
-        String actualTitle = driver.getTitle();
+        String actualTitle = Driver.getDriver().getTitle();
 
         Assert.assertEquals(actualTitle,expectedTitle);
 
